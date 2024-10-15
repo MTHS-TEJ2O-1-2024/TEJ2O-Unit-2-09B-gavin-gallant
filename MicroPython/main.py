46358a40e6bdbc1a4b8710c3c0f25b7ea044e346
gavin-gallant <gavin.gallant@mths.ca>
2024-10-15 13:58:12 +0000

    made scissors icon

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,52 @@
 """
-Created by: Mr. Coxall
+Created by: Gavin Gallant
 Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+This program shows an If Statement
 """
 
 from microbit import *
+import random
 
 
-display.scroll("Hello, World!")
+display.clear()
+display.show(Image.HAPPY)
+
+while True:
+    if accelerometer.was_gesture("shake"):
+        random_number = random.randint(0, 2)
+        display.clear()
+
+        # if random_number was 0
+        if random_number == 0:
+            display.show(Image.SQUARE_SMALL)
+
+        # if random_number was 1
+        if random_number == 1:
+            display.show(Image.SQUARE)
+
+        # if random_number was 2
+        if random_number == 2:
+            display.show(Image.
+                        "00900:"
+                        "09990:"
+                        "00990:"
+                        "00090:"
+                        "00090")
+
+        # pause and show you are ready again
+        sleep(1000)
+        display.show(Image.HAPPY)
+
+
+    if button_a.is_pressed():
+        display.clear()
+        score = score + 1
+        display.show(Image.YES)
+        display.clear()
+        display.show(Image.HAPPY)
+
+
+
+
+
+
